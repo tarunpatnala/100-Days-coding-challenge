7d80a5d5ce1152bea7101a359c31828d8043f101
TarunRajinikanth <tarunpatnala@gmail.com>
2023-09-06 21:01:01 +1000

    10 Days of 100 Days challenge

---
+++ Day20/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
+up = 90
+down = 270
+left = 180
+right = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.snakes = []
+        self.create_snake()
+        self.head = self.snakes[0]
+
+    def create_snake(self):
+        for position in starting_positions:
+            snake = Turtle("square")
+            snake.color("white")
+            snake.penup()
+            snake.goto(position)
+            self.snakes.append(snake)
+
+    def move(self):
+        for x in range(len(self.snakes) - 1, 0, -1):
+            new_pos_x = self.snakes[x - 1].xcor()
+            new_pos_y = self.snakes[x - 1].ycor()
+            self.snakes[x].goto(new_pos_x, new_pos_y)
+        self.head.forward(move_distance)
+
+    def up(self):
+        if self.head.heading() != down:
+            self.head.setheading(up)
+
+    def down(self):
+        if self.head.heading() != up:
+            self.head.setheading(down)
+
+    def left(self):
+        if self.head.heading() != right:
+            self.head.setheading(left)
+
+    def right(self):
+        if self.head.heading() != left:
+            self.head.setheading(right)
